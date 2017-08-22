31b14425789fa5d76f7f0ea34c616fc22c6dbe2c
Ifeoluwa Arowosegbe <iifeoluwa.ao@gmail.com>
2017-08-23 00:49:56 +0100

    Skip evaluating property specified to be ignored.

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ function areObjectPropsSet(objectToTest, propsToIgnore){
 	if (typeof objectToTest === 'object') {
 
 		if (typeof propsToIgnore === 'undefined') {
-				return allPropSet(objectToTest);
+			return allPropSet(objectToTest);
+		}
+		else if(typeof propsToIgnore === 'string'){
+			if (objectToTest.hasOwnProperty(propsToIgnore)) {
+				return somePropSet(objectToTest, propsToIgnore);
 			}
+			
+		}
 	}else{
 		console.log(err);
 	}
@@ -16,6 +22,25 @@ function allPropSet(objectToTest) {
 	let isPropSet = false;
 
 	for (let prop in objectToTest) {
+		if (objectToTest[prop] !== null && objectToTest !== undefined) {
+			isPropSet = true;
+		}else{
+			//property is null or undefined. kill execution
+			return false;
+		}
+	}
+
+	//all props are set, carry on
+	return isPropSet;
+}
+
+function somePropSet(objectToTest, propToIgnore) {
+	let isPropSet = false;
+
+	for (let prop in objectToTest) {
+		//move on to checking next property if current one is to be ignored
+		if (prop === propToIgnore) continue;
+
 		if (objectToTest[prop] !== null && objectToTest !== undefined) {
 			isPropSet = true;
 		}else{
