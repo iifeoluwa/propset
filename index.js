967fd174007822e6340b9e6d3ca9b6d699416eab
Ifeoluwa Arowosegbe <iifeooluwa.ao@gmail.com>
2017-08-23 02:51:42 +0100

    Added DocBlocks

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+ * Verifies that arguments passed to be evaluated are of correct types.
+ * @param  {object} objectToTest  Object which is to be tested to ensure needed props are st
+ * @param  {string|array} propsToIgnore property(ies) of Object to be tested that should be ignored
+ * @return {bool}          result of performing set property checks on the object to be tested
+ */
 function areObjectPropsSet(objectToTest, propsToIgnore){
 		
 	if (typeof objectToTest === 'object' && !Array.isArray(objectToTest)) {
@@ -20,6 +26,11 @@ function areObjectPropsSet(objectToTest, propsToIgnore){
 	}
 }
 
+/**
+ * Check that all properties of the object are set
+ * @param  {object} objectToTest 
+ * @return {bool}
+ */
 function allPropSet(objectToTest) {
 	let isPropSet = false;
 
@@ -36,6 +47,12 @@ function allPropSet(objectToTest) {
 	return isPropSet;
 }
 
+/**
+ * Check that properties of the object are set while ignoring those that should be ignored
+ * @param  {object} objectToTest 
+ * @param  {string | array} propsToIgnore
+ * @return {bool} 
+ */
 function somePropSet(objectToTest, propsToIgnore) {
 	let isPropSet = false;
 
